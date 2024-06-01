5bd5101cd7a4cfdfacf2b18bafdde38703f8bfbf
Andrew Gremlich <agremlich@anonyome.com>
2024-06-01 10:58:02 -0600

    adding html sanitize

--- src/components/NoteEditorView/index.tsx
+++ src/components/NoteEditorView/index.tsx
@@ -2,8 +2,13 @@ import "./ckeditor-styles.css";
 
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { useState } from "react";
+import { Button } from "@headlessui/react";
+import sanitizeHtml from "sanitize-html";
 
 export const Wysiwyg = () => {
+	const [editor, setEditor] = useState<ClassicEditor>();
+
 	return (
 		<div
 			className="m-3 w-full ck-content overflow-x-hidden"
@@ -15,9 +20,12 @@ export const Wysiwyg = () => {
 				onReady={(editor) => {
 					// You can store the "editor" and use when it is needed.
 					console.log("Editor is ready to use!", editor);
+					setEditor(editor);
 				}}
-				onChange={(event) => {
+				onChange={(event, editor) => {
 					console.log(event);
+					const data = editor.getData();
+					console.log({ event, editor, data });
 				}}
 				onBlur={(event, editor) => {
 					console.log("Blur.", editor);
@@ -26,6 +34,20 @@ export const Wysiwyg = () => {
 					console.log("Focus.", editor);
 				}}
 			/>
+			<Button
+				onClick={() => {
+					const data = editor?.getData();
+					const sanitized = sanitizeHtml(data ?? "", {
+						allowedTags: ["p", "strong", "em", "u", "ol", "ul", "li"],
+						allowedAttributes: {
+							a: ["href"],
+						},
+					});
+					console.log(sanitizeHtml(sanitized));
+				}}
+			>
+				Get Data
+			</Button>
 		</div>
 	);
 };
