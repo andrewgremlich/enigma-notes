d3ad9050723e084c9c67708122173c58194f5f16
Andrew Gremlich <agremlich@anonyome.com>
2024-06-01 11:04:44 -0600

    get data from editor

--- src/components/NoteEditorView/index.tsx
+++ src/components/NoteEditorView/index.tsx
@@ -9,42 +9,59 @@ import sanitizeHtml from "sanitize-html";
 export const Wysiwyg = () => {
 	const [editor, setEditor] = useState<ClassicEditor>();
 
+	const getData = () => {
+		const data = editor?.getData();
+		const sanitized = sanitizeHtml(data ?? "", {
+			allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+			allowedAttributes: {
+				a: ["href", "name", "target"],
+				img: ["src", "srcset", "alt", "title", "width", "height", "loading"],
+			},
+			selfClosing: [
+				"img",
+				"br",
+				"hr",
+				"area",
+				"base",
+				"basefont",
+				"input",
+				"link",
+				"meta",
+			],
+		});
+		console.log(sanitizeHtml(sanitized));
+	};
+
 	return (
 		<div
-			className="m-3 w-full ck-content overflow-x-hidden"
+			className="m-3 w-full ck-content overflow-x-hidden "
 			id="content-editor"
 		>
-			<CKEditor
-				editor={ClassicEditor}
-				data="<p>Hello from CKEditor&nbsp;5!</p>"
-				onReady={(editor) => {
-					// You can store the "editor" and use when it is needed.
-					console.log("Editor is ready to use!", editor);
-					setEditor(editor);
-				}}
-				onChange={(event, editor) => {
-					console.log(event);
-					const data = editor.getData();
-					console.log({ event, editor, data });
-				}}
-				onBlur={(event, editor) => {
-					console.log("Blur.", editor);
-				}}
-				onFocus={(event, editor) => {
-					console.log("Focus.", editor);
-				}}
-			/>
+			<div className="mb-4">
+				<CKEditor
+					editor={ClassicEditor}
+					data="<p>Hello from CKEditor&nbsp;5!</p>"
+					onReady={(editor) => {
+						// You can store the "editor" and use when it is needed.
+						console.log("Editor is ready to use!", editor);
+						setEditor(editor);
+					}}
+					onChange={(event, editor) => {
+						console.log(event);
+						const data = editor.getData();
+						console.log({ event, editor, data });
+					}}
+					onBlur={(event, editor) => {
+						console.log("Blur.", editor);
+					}}
+					onFocus={(event, editor) => {
+						console.log("Focus.", editor);
+					}}
+				/>
+			</div>
 			<Button
-				onClick={() => {
-					const data = editor?.getData();
-					const sanitized = sanitizeHtml(data ?? "", {
-						allowedTags: ["p", "strong", "em", "u", "ol", "ul", "li"],
-						allowedAttributes: {
-							a: ["href"],
-						},
-					});
-					console.log(sanitizeHtml(sanitized));
-				}}
+				className={"b-2 bg-blue-500 text-white p-2 rounded-md"}
+				onClick={getData}
 			>
 				Get Data
 			</Button>
