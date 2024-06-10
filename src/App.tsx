fbaad05905ee50f8cbbeded4475bc23887b4edd1
Andrew Gremlich <agremlich@anonyome.com>
2024-06-09 21:12:39 -0600

    learning tauri a bit more

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import "./App.css";
 
 import { useState } from "react";
+import {
+	appCacheDir,
+	appConfigDir,
+	appDataDir,
+	appLogDir,
+	appLocalDataDir,
+} from "@tauri-apps/api/path";
 
 import { FileView } from "./components/FileView";
 import { Settings } from "./components/Settings";
@@ -15,11 +22,28 @@ function App() {
 	//   setGreetMsg(await invoke("greet", { name }));
 	// }
 
+	const showPaths = async () => {
+		const cacheDir = await appCacheDir();
+		const configDir = await appConfigDir();
+		const dataDir = await appDataDir();
+		const logDir = await appLogDir();
+		const localDataDir = await appLocalDataDir();
+
+		console.log("Cache dir:", cacheDir);
+		console.log("Config dir:", configDir);
+		console.log("Data dir:", dataDir);
+		console.log("Log dir:", logDir);
+		console.log("Local data dir:", localDataDir);
+	};
+
 	return (
 		<>
 			<div className="flex justify-center h-full w-full">
 				<FileView />
 				{view === "note" ? <Wysiwyg /> : null}
+				<button type="button" onClick={showPaths}>
+					Show dirs
+				</button>
 				<Settings placeIn="bottom-left" />
 				<NewNote placeIn="bottom-right" />
 			</div>
