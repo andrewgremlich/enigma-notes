7c3d609cfaac5caa90363d150014cd2f5effe51f
Andrew Gremlich <agremlich@anonyome.com>
2024-06-11 20:10:25 -0600

    exploring tauri rust

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,35 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{Builder, PathResolver};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn get_app_dirs() -> String {
+    format!("Just trying another dir")
+}
+
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+    Builder::default()
+        .setup(|app| {
+            // https://docs.rs/tauri/latest/tauri/struct.PathResolver.html#method.app_local_data_dir
+            let path = app.path_resolver();
+            let cache_dir = path.app_cache_dir();
+            let config_dir = path.app_config_dir();
+            let data_dir = path.app_data_dir();
+            let log_dir = path.app_log_dir();
+            let local_data_dir = path.app_local_data_dir();
+
+            println!("Path: {:?}", data_dir);
+
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![greet, get_app_dirs])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,9 @@ import {
 	appDataDir,
 	appLogDir,
 	appLocalDataDir,
+	cacheDir as cacheDir2,
 } from "@tauri-apps/api/path";
+import { invoke } from '@tauri-apps/api/tauri'
 
 import { FileView } from "./components/FileView";
 import { Settings } from "./components/Settings";
@@ -17,10 +19,14 @@ import type { NoteView } from "./types/views";
 
 function App() {
 	const [view, setView] = useState<NoteView>("note");
-	// async function greet() {
-	// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-	//   setGreetMsg(await invoke("greet", { name }));
-	// }
+
+	async function appDirs() {
+		// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+		console.log(await invoke("get_app_dirs"));
+		console.log(await invoke("greet", { name: "World" }));
+	}
+
+	appDirs();
 
 	const showPaths = async () => {
 		const cacheDir = await appCacheDir();
@@ -28,12 +34,14 @@ function App() {
 		const dataDir = await appDataDir();
 		const logDir = await appLogDir();
 		const localDataDir = await appLocalDataDir();
+		const cacheDir2string = await cacheDir2();
 
 		console.log("Cache dir:", cacheDir);
 		console.log("Config dir:", configDir);
 		console.log("Data dir:", dataDir);
 		console.log("Log dir:", logDir);
 		console.log("Local data dir:", localDataDir);
+		console.log("Cache dir 2:", cacheDir2string);
 	};
 
 	return (
