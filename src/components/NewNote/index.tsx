ace3705227c7e272a10eb853656ee7bf105eb21f
Andrew Gremlich <agremlich@anonyome.com>
2024-06-18 13:17:38 -0600

    nevermind. keeping the options out of the way

--- src/components/NewNote/index.tsx
+++ src/components/NewNote/index.tsx
@@ -1,4 +1,10 @@
-import { Button } from "@headlessui/react";
+import {
+	Button,
+	Menu,
+	MenuButton,
+	MenuItem,
+	MenuItems,
+} from "@headlessui/react";
 import {
 	FiAlignLeft,
 	FiBarChart2,
@@ -8,37 +14,56 @@ import {
 	FiPenTool,
 	FiTable,
 } from "react-icons/fi";
-
+import { FaPencilAlt } from "react-icons/fa";
 import { IconStyle, type PlaceIn, placeInStyle } from "@/utils/style";
 import type { NoteView } from "@/types/views";
-
 const IconSize = 30;
-
 export const NewNote = ({
 	placeIn,
 	activateView,
 }: { placeIn: PlaceIn; activateView: (view: NoteView) => void }) => (
 	<div className={`${placeInStyle(placeIn)} flex flex-col-reverse`}>
-		<Button onClick={() => activateView("note")}>
-			<FiAlignLeft size={IconSize} className={`${IconStyle} m-4`} />
-		</Button>
-		<Button onClick={() => activateView("calendar")}>
-			<FiCalendar size={IconSize} className={`${IconStyle} m-4`} />
-		</Button>
-		<Button onClick={() => activateView("table")}>
-			<FiTable size={IconSize} className={`${IconStyle} m-4`} />
-		</Button>
-		<Button onClick={() => activateView("draw")}>
-			<FiPenTool size={IconSize} className={`${IconStyle} m-4`} />
-		</Button>
-		<Button onClick={() => activateView("music")}>
-			<FiMusic size={IconSize} className={`${IconStyle} m-4`} />
-		</Button>
-		<Button onClick={() => activateView("chart")}>
-			<FiBarChart2 size={IconSize} className={`${IconStyle} m-4`} />
-		</Button>
-		<Button onClick={() => activateView("map")}>
-			<FiMap size={IconSize} className={`${IconStyle} m-4`} />
-		</Button>
+		<Menu>
+			<MenuButton>
+				<FaPencilAlt size={IconSize} />
+			</MenuButton>
+			<MenuItems anchor="bottom">
+				<MenuItem>
+					<Button onClick={() => activateView("note")}>
+						<FiAlignLeft size={IconSize} className={`${IconStyle} m-4`} />
+					</Button>
+				</MenuItem>
+				<MenuItem>
+					<Button onClick={() => activateView("calendar")}>
+						<FiCalendar size={IconSize} className={`${IconStyle} m-4`} />
+					</Button>
+				</MenuItem>
+				<MenuItem>
+					<Button onClick={() => activateView("table")}>
+						<FiTable size={IconSize} className={`${IconStyle} m-4`} />
+					</Button>
+				</MenuItem>
+				<MenuItem>
+					<Button onClick={() => activateView("draw")}>
+						<FiPenTool size={IconSize} className={`${IconStyle} m-4`} />
+					</Button>
+				</MenuItem>
+				<MenuItem>
+					<Button onClick={() => activateView("music")}>
+						<FiMusic size={IconSize} className={`${IconStyle} m-4`} />
+					</Button>
+				</MenuItem>
+				<MenuItem>
+					<Button onClick={() => activateView("chart")}>
+						<FiBarChart2 size={IconSize} className={`${IconStyle} m-4`} />
+					</Button>
+				</MenuItem>
+				<MenuItem>
+					<Button onClick={() => activateView("map")}>
+						<FiMap size={IconSize} className={`${IconStyle} m-4`} />
+					</Button>
+				</MenuItem>
+			</MenuItems>
+		</Menu>
 	</div>
 );
