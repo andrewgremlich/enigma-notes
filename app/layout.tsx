ee68b337012c56915468ceaa46e3885bc9ad25fe
Andrew Gremlich <agremlich@anonyome.com>
2024-07-24 17:38:06 -0600

    modifying notes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,11 @@ import Providers from "@/components/ReactQueryClientProvider";
 
 import "./globals.css";
 
-// TODO: Make Settings it's own page.
+// TODO: warn if no crypto key is set on the editor page.
+
+// TODO: location metadata for notes.
+
+// TODO: Weather metadata for notes.
 
 // TODO: Mobile friendly.
 
@@ -12,23 +16,15 @@ import "./globals.css";
 
 // TODO: double check potential recovery options? Do I need to store password hash and salt anywhere? prompt the user to save the salt and hash?
 
-// TODO: warn if no crypto key is set on the editor page.
-
-// TODO: location metadata for notes.
-
-// TODO: Weather metadata for notes.
-
 // TODO: you have a folder structure existing, change it to be the many journals or tags. set up a hierarchal structure of notes.
+// TODO: be sure to make a breadcrumb trail with tags or whatever it'll be
 
-// TODO: be sure to make a breadcrumb trail.
-
-// TODO: make note editor functioning.
+// TODO: make note editor functioning. need a toolbar. need a way to save the note. need a way to encrypt the note. need a way to decrypt the note.
 
 // TODO: support pictures and note linking.
 // https://dev.to/koralarts/slatejs-adding-images-and-links-2g93
 // https://github.com/ianstormtaylor/slate/blob/main/site/examples/images.tsx
 
-// TODO: a way to write the file in the right spot. then think of a way for encryption.
 // TODO: and exporter to MD, Word, PDF
 
 export const metadata: Metadata = {
