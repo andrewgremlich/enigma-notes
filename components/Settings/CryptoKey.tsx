8a5251f457885de5fcab343dfb8aa8af4c486d69
andrewgremlich <andrew.gremlich@gmail.com>
2024-08-09 23:11:34 -0600

    adding crypto key settings

--- components/Settings/CryptoKey.tsx
+++ components/Settings/CryptoKey.tsx
@@ -16,9 +16,15 @@ export const CryptoKeySettings = () => {
     <div>
       <h2>Crypto Key Settings</h2>
 
+      <h3>Local Key</h3>
+
       <p>Key does {!isLoading && data ? "exist" : "does not exist."}</p>
 
       <PrimaryButton>Refresh Key</PrimaryButton>
+
+      <h3>Contact Key</h3>
+
+      <p>Share this with people you want to share documents with.</p>
     </div>
   );
 }
